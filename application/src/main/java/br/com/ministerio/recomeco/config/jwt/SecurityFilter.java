c6c3ed15ea40e7fec27e5d5831e77553688d88ac
Kleyvisson Matias <kleyvissonmatias@gmail.com>
2023-09-27 18:23:16 -0300

    - Melhoria de código;

--- application/src/main/java/br/com/ministerio/recomeco/config/jwt/SecurityFilter.java
+++ application/src/main/java/br/com/ministerio/recomeco/config/jwt/SecurityFilter.java
@@ -22,11 +22,13 @@ public class SecurityFilter extends OncePerRequestFilter {
     UsuarioRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if (token != null) {
             var login = tokenService.validateToken(token);
-            UserDetails user  = userRepository.obterUsername(login);
+            UserDetails user = userRepository.obterUsername(login);
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
